106600ea8b3b4526e4b5b44ee88534e9af47a22f
Cobra Kao <ru6xul6@gmail.com>
2014-02-05 20:35:22 -0800

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,11 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+  $(".friend a").click(function(event) {
+    event.preventDefault();
+    var name = $(this).text();
+    $(this).text(anagrammedName(name));
+  });
 })
 
 /*
